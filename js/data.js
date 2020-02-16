db3f8044dd1c57a84e9059f205ec5ab85055801c
cneidson <carrie.eidson@nyu.edu>
2020-02-16 13:40:49 -0500

    Initial Push

---
+++ js/data.js
@@ -0,0 +1,99 @@
+var restaurData = [
+  {
+    "name": "Otafuku",
+    "borough": "Manhatten",
+    "longitude": -73.988463,
+    "latitude": 40.729659,
+    "address":"220 E 9th St",
+    "website":"otafukuny.com",
+    "phone": "(646) 998-3438",
+  },
+  {
+    "name": "Sunrise Mart",
+    "borough": "Manhatten",
+    "longitude": -73.980736,
+    "latitude": 40.752396,
+    "address":"12 E 41st St",
+    "website":"facebook.com/sunrisemartmidtown",
+    "phone": "(646) 380-9280",
+  },
+  {
+    "name": "Ivan Ramen",
+    "borough": "Manhatten",
+    "longitude": -73.9845,
+    "latitude": 40.720777,
+    "address":"25 Clinton St",
+    "website":"ivanramen.com",
+    "phone": "(646) 678-3859",
+  },
+  {
+    "name": "Shalom Japan",
+    "borough": "Brooklyn",
+    "longitude": -73.955794,
+    "latitude": 40.709352,
+    "address":"310 S 4th St",
+    "website":"shalomjapannyc.com",
+    "phone": "(718) 388-4012",
+  },
+  {
+    "name": "Bar Goto",
+    "borough": "Manhatten",
+    "longitude": -73.989525,
+    "latitude": 40.722992,
+    "address":"245 Eldridge St",
+    "website":"bargoto.com",
+   "phone": "(212) 475-4411",
+
+  },
+  {
+    "name": "HanaMichi",
+    "borough": "Manhatten",
+    "longitude": -73.987024,
+    "latitude": 40.747828,
+    "address":"28 W 32nd St",
+    "website":"tinyurl.com/t6o2b8u",
+    "phone":"(212) 736-5393",
+
+  },
+  {
+    "name": "Kenka ",
+    "borough": "Manhatten",
+    "longitude": -73.988464,
+    "latitude": 40.729143,
+    "address":"25 St Marks Pl",
+    "website":"kenkanyc.com",
+    "phone": "(212) 254-6363",
+
+  },
+  {
+    "name": "DokoDomo",
+    "borough": "Manhatten",
+    "longitude": -73.989645,
+    "latitude": 40.726584,
+    "address":"89 E 4th St",
+    "website":"dokodemonyc.com",
+    "phone": "(917) 261-5228",
+
+
+  },
+  {
+    "name": "Donburiya",
+    "borough": "Manhatten",
+    "longitude": -73.983025,
+    "latitude": 40.766292,
+    "address":"253 W 55th St",
+    "website":"donburiyany.com",
+    "phone": "(212) 980-7909",
+
+  },
+  {
+    "name": "Mu Ramen",
+    "borough": "Queens",
+    "longitude": -73.950659,
+    "latitude": 40.744035,
+    "address": "1209 Jackson Ave",
+    "website":"muramennyc.com",
+    "phone":"(718) 707-0098",
+
+  }
+]
---
+++ js/scripts.js
@@ -0,0 +1,107 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY25laWRzb24iLCJhIjoiY2s2b2dseXRqMTd6NzNrbzZhM20wa240cCJ9.Ba3dfLpzNZaxw1izIYezpw';
+
+
+// we want to return to this point and zoom level after the user interacts
+// with the map, so store them in variables
+var initialCenterPoint = [-73.987, 40.735]
+var initialZoom = 11.67
+
+
+// create an object to hold the initialization options for a mapboxGL map
+var initOptions = {
+  container: 'map-container', // put the map in this container
+  style: 'mapbox://styles/mapbox/light-v10', // use this basemap
+  center: initialCenterPoint, // initial view center
+  zoom: initialZoom, // initial view zoom level (0-18)
+}
+
+// create the new map
+var map = new mapboxgl.Map(initOptions);
+
+// add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
+
+restaurData.forEach(function(restaurEntry) {
+  new mapboxgl.Marker()
+    .setLngLat([restaurEntry.longitude, restaurEntry.latitude])
+    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    .setHTML(`<div class="markerbg">${restaurEntry.name}</div>
+              <p><b>Address:</b> ${restaurEntry.address}<br>
+              <b>Website:</b> <a href="http://www.${restaurEntry.website}" target="_blank">${restaurEntry.website}</a><br>
+              <b>Phone:</b> ${restaurEntry.phone}</p>`))
+    .addTo(map);
+})
+
+
+
+$('#otafuku').on('click', function() {
+  map.flyTo({
+    center: [-73.988463, 40.729659],
+    zoom: 19
+  })
+})
+
+$('#sunrise').on('click', function() {
+  map.flyTo({
+    center: [-73.980736, 40.752396],
+    zoom: 16
+  })
+})
+
+$('#ivan').on('click', function() {
+  map.flyTo({
+    center: [-73.9845, 40.720777],
+    zoom: 16
+  })
+})
+
+$('#bargoto').on('click', function() {
+  map.flyTo({
+    center: [-73.989525, 40.722992],
+    zoom: 16
+  })
+})
+
+$('#shalom').on('click', function() {
+  map.flyTo({
+    center: [-73.955794, 40.709352],
+    zoom: 16
+  })
+})
+
+$('#kenka').on('click', function() {
+  map.flyTo({
+    center: [-73.988464, 40.729143],
+    zoom: 19
+  })
+})
+
+$('#hanamichi').on('click', function() {
+  map.flyTo({
+    center: [-73.987024, 40.747828],
+    zoom: 16
+  })
+})
+
+$('#donburiya').on('click', function() {
+  map.flyTo({
+    center: [-73.983025, 40.766292],
+    zoom: 16
+  })
+})
+
+
+$('#muramen').on('click', function() {
+  map.flyTo({
+    center: [-73.950659, 40.744035],
+    zoom: 16
+  })
+})
+
+
+$('#dokodomo').on('click', function() {
+  map.flyTo({
+    center: [-73.989645, 40.726584],
+    zoom: 16
+  })
+})
